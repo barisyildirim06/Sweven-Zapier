b5a3946afb927d110a03cc744ffe1573687fc66f
barisyildirim06 <yildrmbaris@gmail.com>
2021-12-04 18:49:12 +0300

    Add site sample

---
+++ src/samples/site.js
@@ -0,0 +1,23 @@
+const siteSample = {
+    id: 11953,
+    number: '00001',
+    name: 'Demo Site',
+    address: '1011 W Central Blvd',
+    city: 'Orlando',
+    state: 'FL',
+    zip: '10001',
+    phone: '212-736-3100',
+    client_line_business: 'CLB Demo',
+    program_category: null,
+    region_id: null,
+    district_id: null,
+    client_id: 3041,
+    owner_leased: 3,
+    square_feet: 300,
+    latitude: '40.748784',
+    longitude: '-73.986158',
+    created_by: 749,
+    created_date: '2021-11-26 14:10:22.497000',
+    inactive: false
+}
+module.exports = { siteSample }
