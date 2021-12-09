8a04154f6b5a4e32155a5face384f6d18b753de2
barisyildirim06 <yildrmbaris@gmail.com>
2021-12-10 00:15:59 +0300

    Implement sub project sample

---
+++ src/samples/sub-project.js
@@ -0,0 +1,15 @@
+const subProjectSample = {
+    id: 311,
+    name: 'Demo Sub Project metu',
+    description: 'Demo Sub Project',
+    client_id: 3041,
+    project_id: 301,
+    division_id: 784,
+    status_id: 1846,
+    project_manager_id: 749,
+    eta: '1900-01-01 00:00:00',
+    created_by: 749,
+    created_date: '2021-11-26 14:10:22.533000',
+    inactive: true
+}
+module.exports = { subProjectSample };
