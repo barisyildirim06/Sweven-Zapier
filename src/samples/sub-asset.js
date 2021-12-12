38df9043b2592326342a72ee8dee2e1353efea21
barisyildirim06 <yildrmbaris@gmail.com>
2021-12-12 12:30:15 +0300

    Implement sub asset sample

---
+++ src/samples/sub-asset.js
@@ -0,0 +1,23 @@
+const subAssetSample = {
+    id: 1,
+    name: "Prueba manual",
+    sub_asset_id: 1,
+    site_id: 153,
+    asset_id: 138,
+    state: "DN",
+    asset_type_id: 2668,
+    make: "111",
+    model: "11",
+    serial_number: "11",
+    location: "11",
+    size: "11",
+    unit_size: "11",
+    given_beginning_date: "09/22/2021",
+    given_end_date: "09/22/2021",
+    install_date: "09/22/2021",
+    life_time_remaining: "11",
+    warranty_vendor: "11",
+    telecordia: "111",
+    barcode: ""
+}
+module.exports = { subAssetSample }
