d21f3d53dfca7d906c237c2d51e82605cecfcdb9
barisyildirim06 <barisyildirim06@MacBook-Pro.local>
2022-01-25 20:00:49 +0300

    Modify help text of auth

--- src/input-fields/auth/authentication.js
+++ src/input-fields/auth/authentication.js
@@ -3,14 +3,14 @@ const authenticationInputFields = [
         key: 'email',
         type: 'string',
         required: true,
-        helpText: `You can use your email credentials for https://devapp.swevenbpm.com/`
+        helpText: `You can use your email credentials for https://devapp.swevenbpm.com/ .`
 
     },
     {
         key: 'password',
         type: 'password',
         required: true,
-        helpText: `You can use your password credentials for https://devapp.swevenbpm.com/`
+        helpText: `You can use your password credentials for https://devapp.swevenbpm.com/ .`
     }
 ]
 module.exports = { authenticationInputFields }
